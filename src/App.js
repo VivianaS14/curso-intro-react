7f701ea8b7704384e2cb37940a20d888eb3af6d5
VivianaS14 <vasorianp@gmail.com>
2022-12-15 21:43:18 -0500

    Estructura de cada componente

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch";
+import { TodoList } from "./components/TodoList";
+import { TodoItem } from "./components/TodoItem";
+import { CreateTodoButton } from "./components/CreateTodoButton";
+//import './App.css';
+
+//Lista falsa de TODOs
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'LALALAL', completed: false },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Etiqueta invisible
+    <>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map(todo => (<TodoItem key={todo.text} text={todo.text} />))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </>
   );
 }
 
